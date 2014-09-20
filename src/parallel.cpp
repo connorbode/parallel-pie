2304ec7c7b9fd5b8c1ef98445568ce4b80db9e89
Connor Bode <connor.bode@gmail.com>
2014-09-20 13:14:06 -0400

    fix timer -- algorithm finished.
    #2

--- src/parallel.cpp
+++ src/parallel.cpp
@@ -26,6 +26,8 @@ int main (int argc, char const *argv[]) {
                   ms_elapsed, // the length of the computation in milliseconds
                   us_elapsed; // the length of the computation in microseconds
 
+  bool            timer_started = false;
+
   // check that number of total tries is suppplied
   try {
     total_tries = atoi(argv[1]);
@@ -34,17 +36,19 @@ int main (int argc, char const *argv[]) {
     exit(0);
   }
 
-  total_tries = 100000000;
-
   // seed rand
   srand(time(NULL));
 
-  // start timer and computation
-  gettimeofday(&start_time, NULL);
+  // start timer
+  if (!timer_started) {
+    timer_started = true;
+    gettimeofday(&start_time, NULL);
+  }
+
+  // start computation
   MPI_Init(NULL, NULL);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
   std::cout << "I'm a process, and my rank is " << rank << "\n";
 
   // run this processes' share of the computation
@@ -54,18 +58,22 @@ int main (int argc, char const *argv[]) {
   // reduce all computations and generate pi
   reduce = MPI_Reduce(&hits, &total_hits, 1, MPI_INT, MPI_SUM, ROOT, MPI_COMM_WORLD);
   if (reduce != MPI_SUCCESS) std::cout << "MPI_Reduce failed..";
-  if (rank == ROOT) pi = generatePi(hits, tries);
+
+  // output result and time difference
+  if (rank == ROOT) {
+    pi = generatePi(total_hits, total_tries);
+    gettimeofday(&end_time, NULL);
+    s_elapsed = end_time.tv_sec - start_time.tv_sec;
+    ms_elapsed = end_time.tv_usec / 1000 - start_time.tv_usec / 1000;
+    us_elapsed = end_time.tv_usec - start_time.tv_usec;
+    std::cout << "Tries: " << total_tries << "\n";
+    std::cout << "Hits: " << total_hits << "\n";
+    std::cout << "PI: " << pi << "\n";
+    std::cout << "Time elapsed: " << us_elapsed << " microseconds; " << ms_elapsed << " milliseconds; " << s_elapsed << " seconds\n";
+  }
 
   // end timer and MPI
   MPI_Finalize();
-  gettimeofday(&end_time, NULL);
-
-  // output result and time difference
-  s_elapsed = end_time.tv_sec - start_time.tv_sec;
-  ms_elapsed = end_time.tv_usec / 1000 - start_time.tv_usec / 1000;
-  us_elapsed = end_time.tv_usec - start_time.tv_usec;
-  std::cout << "PI: " << pi << "\n";
-  std::cout << "Time elapsed: " << us_elapsed << " microseconds; " << ms_elapsed << " milliseconds; " << s_elapsed << " seconds\n";
 
   return 0;
 }
